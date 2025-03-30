599adec10eeab5dc7a8eaf56265240510383d8c6
Allen Avramov <allenjob96@gmail.com>
2025-03-30 21:00:32 +0300

    added abutton for a second Branch

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.getElementById("SuccessButton").addEventListener("click", () =>{
     alert("Success!!! .... not really")
 });
+
+document.getElementById("FailButton").addEventListener('click', () => {
+    alert("You fail!!!....  go back to school")
+})
